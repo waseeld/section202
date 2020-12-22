50b3920d9efc0651723808aa37a5dd8b305377dd
Wasel Al-Mahri <33339924+waseeld@users.noreply.github.com>
2020-12-22 18:33:52 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// if (
+//   window.location.pathname == "/exams" ||
+//   window.location.pathname == "/exams/"
+// ) {
+//   window.location = "https://u308r.csb.app/exams/index.html";
+// }
+new TypeIt("#coming-soon", {
+  strings: ["الموقع تحت الصيانة", "...", "إنتظرونا قريبا"]
+}).go();
